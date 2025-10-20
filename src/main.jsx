421fb493777ed23ae5f134007e2ff5eb3677a6e4
rishita2410 <isroscience2@gmail.com>
2025-10-20 20:11:20 +0530

    corrected the routing problem of page2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Page1 from "./page1.jsx";
+import Page2 from "./page2.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
--- src/page1.jsx
+++ src/page1.jsx
@@ -1,13 +1,18 @@
 import Bg from "./components/bg"
 import Heading from './components/heading'
+import React from "react";
+import {Link} from 'react-router-dom'
 import './page1.css'
+import './page2.jsx'
 function Page1(){
   return(
     <div>
     <Bg></Bg>
     <Heading></Heading>
     <p id="p1-main">Our Home in the World of Comics</p>
-    <a id = "p1-end" href="C:\Users\ASUS\Desktop\Comics_Lab\ComicsStudiesLab\src\page2.jsx">Enter</a>
+    {/* <a id = "p1-end" href="C:\Users\ASUS\Desktop\Comics_Lab\ComicsStudiesLab\src\page2.jsx"><button>Enter</button></a> */}
+    <Link id="p1-end" to="/page2">Enter</Link>
+
     
 
     </div>
